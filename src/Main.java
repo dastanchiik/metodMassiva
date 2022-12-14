bc37253111daf1ffdc710ba6292409f3141d5a13
dastanchick <dosyafree32@gmail.com>
2022-12-14 18:24:31 +0600

    I am added words

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      Class clas = new Class(1, "maths",new int[]{123,12,14,43,34});
-        System.out.println(clas);
+        for (int i = 1; i <= ints().length; i++) {
+            System.out.print(i+",");
+        }
+    }
+    public static int[] ints(){
+        return new int[]{1,2,3,4,5};
     }
 }
