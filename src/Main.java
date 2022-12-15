bbd79cfd2e62826c50a43bbb7fc55039ddbce0a8
daniel12142007 <danijelahatzanov@gmail.com>
2022-12-15 17:10:06 +0600

    I add

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
        Scanner scanner = new Scanner(System.in);
+        List<People>people=new ArrayList<>();
+        while(true){
+
+        }
     }
 }
