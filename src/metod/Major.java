3197ada9fb28217f0e4c1f9ff480700d59c1659d
dastanchick <dosyafree32@gmail.com>
2022-12-25 17:57:17 +0600

    I am added words

--- src/metod/Major.java
+++ src/metod/Major.java
@@ -6,15 +6,26 @@ public class Major {
     public static void main(String[] args) {
         method();
     }
-    public static void method(){
+
+    public static void method() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("1 -> name  \n2 -> age \n3 -> name and age");
-
-        while (true){
-            if (){
-        Kubat kubat = new Kubat("Kubat",9);
-        System.out.println(kubat);
-        }
+        int a = scanner.nextInt();
+        Kubat kubat = new Kubat("Kubat", 9);
+        while (true) {
+            if (a == 1) {
+                System.out.println(kubat.name());
+                System.out.println("1 -> name  \n2 -> age \n3 -> name and age");
+                a = scanner.nextInt();
+            } else if (a == 2) {
+                System.out.println(kubat.age());
+                System.out.println("1 -> name  \n2 -> age \n3 -> name and age");
+                a = scanner.nextInt();
+            } else if (a == 3) {
+                System.out.println(kubat);
+                System.out.println("1 -> name  \n2 -> age \n3 -> name and age");
+            a = scanner.nextInt();
+            }
         }
     }
 }
