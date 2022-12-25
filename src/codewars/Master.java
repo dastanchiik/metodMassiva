62e661b4e81d538a4b21ceee671af44e81e1f5c2
dastanchick <dosyafree32@gmail.com>
2022-12-25 21:10:03 +0600

    I am added words

--- src/codewars/Master.java
+++ src/codewars/Master.java
@@ -7,18 +7,20 @@ import java.util.Scanner;
 
 public class Master {
     public static void main(String[] args) {
-        method();
+        method("Dastan", "fan");
     }
 
-    public static void method() {
-       Scanner scanner = new Scanner(System.in);
-       String name = "abc";
-       String sub = name.substring(1);
-       String name2 = scanner.next();
-       if (name2.equals(sub)){
-        System.out.println("true");
-    }else {
-           System.out.println("false");
-       }
+    public static void method(String str, String ending) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(str + "\n" + ending);
+        int st = ending.length();
+        int count = st - 1;
+        int counter = st - count;
+        String string = str.substring(st);
+        if (ending.equals(string)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
