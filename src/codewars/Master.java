1cd3e99876d0b7f54dd0dc5d87749f091ee43ee4
dastanchick <dosyafree32@gmail.com>
2022-12-26 14:36:00 +0600

    I am added words

--- src/codewars/Master.java
+++ src/codewars/Master.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 public class Master {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        method("Dastan", "fan");
+        method("Dastan", "an");
     }
 
     public static void method(String str, String ending) {
-        String expected = str.substring(ending.length());
-        if (ending.equals(expected)) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+        System.out.println(str.endsWith(ending));
     }
 }
