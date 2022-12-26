96ee73425e310893cec1581a465281a8a17c8cec
dastanchick <dosyafree32@gmail.com>
2022-12-26 16:09:56 +0600

    I am added words

--- src/codewars/Master.java
+++ src/codewars/Master.java
@@ -1,14 +1,32 @@
 package codewars;
 
+import java.nio.file.FileSystemNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Master {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        method("Dastan", "an");
+        method();
     }
 
-    public static void method(String str, String ending) {
-        System.out.println(str.endsWith(ending));
+    public static void method() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Cat>groupCats = new ArrayList<>();
+        groupCats.add(new Cat("Borya",3));
+        groupCats.add(new Cat("Sima",1));
+        groupCats.add(new Cat("Netflix",2));
+        System.out.print("Enter name:");
+        String name = scanner.next();
+        System.out.print("Enter age:");
+        int age = scanner.nextInt();
+        for (Cat e:groupCats) {
+            if (e.getAge()==age && e.getName().equals(name)){
+                System.out.println("Cats in group");
+                System.out.println(groupCats.contains(e));
+                System.out.println(groupCats.indexOf(e));
+            } else if (e.getName().equals("Sima") && e.getAge()==1) {
+
+            }
+        }
     }
 }
